d1f991a40157a8722fe03247cd9ae85997c52b99
dinhductai <dinhductai2501@gmail.com>
2025-10-02 16:47:38 +0700

    init: start project

---
+++ smart-schedule/smart-schedule/src/main/java/com/example/smart_schedule/SmartScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartScheduleApplication.class, args);
+	}
+
+}
---
+++ smart-schedule/smart-schedule/src/test/java/com/example/smart_schedule/SmartScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
